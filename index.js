ac69672b38bcc36d6be0586f7eaba94c4b5e5da4
cfdebug <rfadventure22@gmail.com>
2022-06-16 19:35:18 -0500

    Added console log for user's name and modified popup text

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ let playagain = true;
 
 // Main Function
 function playgame() {
+    //Log Username
+    console.log("Current User is: "+username);
 //For loop to display/answer questions and track score    
 for (let i = 0; i < questions.length; i++) {
     let question = questions[i];
     let useranswer = window.prompt(question.text);
     if(useranswer === question.correctAnswer){
+        console.log("Correct");
         userscore = userscore + 10;
+    } else {
+        console.log("Incorrect")
     };
 };
 // Score popup
-window.alert("Your score is: "+userscore);
+window.alert(username+"'s score is: "+userscore);
 }
 //Play again popup
 while (playagain === true){
